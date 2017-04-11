8c6b4d89ec8257a0e4feac82a737028517472088
Emily <e.a.moses@gmail.com>
2017-04-10 20:10:09 -0700

    adding external html template to be served by go

--- chat/main.go
+++ chat/main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"text/template"
+	"path/filepath"
 )
 
-func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte (`
-			<html>
-				<head>
-					<title>
-						Chat
-					</title>
-				</head>
-				<body>
-					Let's chat!
-				</body>
-			</html>
-		`))
-	})
+type templateHandler struct {
+	once sync.Once
+	filename string
+	templ *template.Template //templ represents a single template
 }
 
-//start the web server
-// if err := http.ListenAndServe(":8080", nil)
-// 	err != nil {
-// 	log.Fatal("ListenAndServe: ", err)
-// }
+//ServeHTTP handles HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func(){
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main(){
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	//start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
