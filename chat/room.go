d1bacd24919f170cc9c0506132a5cd54c5300463
Emily <e.a.moses@gmail.com>
2017-04-18 13:15:07 -0700

    adding client and room types

---
+++ chat/room.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+
+)
+
+type room struct {
+	//forward is a channel that holds incoming messages that should be forwarded to other clients
+	forward chan []byte
+}
+
+func main(){
+
+ }
