bf78f7c006aaf966fe063087544e8c3ee0ec4b7b
Emily <e.a.moses@gmail.com>
2017-04-21 19:54:36 -0700

    additions to the room

--- chat/room.go
+++ chat/room.go
@@ -1,14 +1,68 @@
 package main
 
 import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
 )
 
 type room struct {
-	//forward is a channel that holds incoming messages that should be forwarded to other clients
+	// forward is a channel that holds incoming messages that should be forwarded to other clients
+	// join is a channel for clients whishing to join the room
+	// leave is a channel for clients wishing to leave the room
+	// clients holds all current clinest in this room
 	forward chan []byte
+	join chan *client
+	leave chan *client
+	clients map[*client]bool
 }
 
-func main(){
+func (r *room) run()  {
+	for {
+		select {
+		case client := <-r.join:
+			//joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			//leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			//forward message to all clients
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					//send the messages
+				default:
+					//failed to send
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
 
- }
+const (
+	socketBufferSize = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request)  {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send: make(chan []byte, messageBufferSize),
+		room: r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client } ()
+	go client.write()
+	client.read()
+}
