cb4460c2fbf43e43169ab630802e30627497d4d1
Emily <e.a.moses@gmail.com>
2017-05-14 13:17:06 -0700

    Adding auth to chat app

---
+++ chat/auth.go
@@ -0,0 +1,25 @@
+package main
+
+import "net/http"
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		//NOT authenticated
+		w.Header().set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		//ERROR
+		panic(err.Error())
+	} else {
+		//SUCCESS
+		h.next.ServeHTTP(w, r)
+	}
+ }
+
+ func MustAuth(handler http.Handler) http.Handler {
+	 return &authHandler{next: handler}
+ }
